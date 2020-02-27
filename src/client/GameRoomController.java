c7cc67eec03ffe8c86bd966878689f2d80045b60
Alexander Crosby <axc1153@cs.bham.ac.uk>
2020-02-27 09:34:12 +0000

    Changed Username list to labels rather than text

--- src/client/GameRoomController.java
+++ src/client/GameRoomController.java
@@ -2,6 +2,7 @@ package client;
 
 import javafx.application.Platform;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
@@ -47,9 +48,9 @@ public class GameRoomController {
     public void updateUsers(String[] users) {
         Platform.runLater(() -> userList.getChildren().clear());
         for (String user : users) {
-            TextField userName = new TextField();
+            Label userName = new Label();
             userName.setText(user);
-            userName.setEditable(false);
+            //userName.setEditable(false);
             Platform.runLater(() -> userList.getChildren().add(userName));
         }
         System.out.println("OK");
