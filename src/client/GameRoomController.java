d352b035ed1668a0075eccd0266c2dcb110ed388
lxj973 <lxj973@cs.bham.ac.uk>
2020-03-09 16:00:56 +0000

    beautify the game room ui

--- src/client/GameRoomController.java
+++ src/client/GameRoomController.java
@@ -1,5 +1,9 @@
 package client;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,10 +12,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -21,12 +32,6 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import messaging.Command;
 import messaging.Coordinate;
-import javafx.scene.control.ColorPicker;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
 
 public class GameRoomController implements Initializable {
     @FXML
@@ -49,6 +54,10 @@ public class GameRoomController implements Initializable {
     private Button clearButton;
     @FXML
     private Text wordToDraw;
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private BorderPane root;
     private Client client;
     private Stage stage;
     private GraphicsContext gc;
@@ -105,6 +114,7 @@ public class GameRoomController implements Initializable {
         });
         clearCanvas();
         client.sendMessage(Command.REQUEST_USERS);
+  
     }
 
     /**
--- src/client/HomeController.java
+++ src/client/HomeController.java
@@ -1,23 +1,20 @@
 package client;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class HomeController implements Initializable {
     private HashMap<Text, String> textRoom;
@@ -58,15 +55,16 @@ public class HomeController implements Initializable {
     }
 
     public void roomScene() {
-        Parent root = null;
+        BorderPane root = null;
         try {
             root = FXMLLoader.load(getClass().getResource("GameRoom.fxml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Scene roomScene = new Scene(root);
+        Scene roomScene = new Scene(root);      
         stage.setScene(roomScene);
         roomScene.getStylesheets().add(getClass().getResource("CreatAccountStyle" + ".css").toExternalForm());
+        stage.setResizable(false);
         stage.show();
     }
 
